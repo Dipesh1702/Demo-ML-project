c90accf7b7a68b1ab9338fe22464b2064c05bd20
Dipesh <dipeshpatil1702@gmail.com>
2024-02-03 17:28:00 +0530

    data ingestion

---
+++ src/mlproject/components/data_ingestion.py
@@ -0,0 +1,42 @@
+import os
+import sys
+from src.mlproject.exception import CustomException
+from src.mlproject.logger import logging
+import pandas as pd
+from dataclasses import dataclass
+from src.mlproject.utils import read_sql_data
+from sklearn.model_selection import train_test_split
+
+
+@dataclass
+class DataingestionConfig:
+    train_data_path:str=os.path.join('artifacts','train.csv')
+    test_data_path:str=os.path.join('artifacts','test.csv')
+    rav_data_path:str=os.path.join('artifacts','rav.csv')
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataingestionConfig()
+
+    def initiate_data_ingestion(self):
+        try:
+            df=read_sql_data()
+            logging.info("reading complated mysql database")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.rav_data_path,index=False,header=True)
+            train_set,test_set=train_test_split(df,test_size=0.25,random_state=1)
+            df.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            df.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info("data ingestion complited")
+
+            return (self.ingestion_config.train_data_path,
+                    self.ingestion_config.test_data_path
+                    )
+
+            
+        except Exception as e:
+            raise CustomException(e,sys)
--- src/mlproject/exception.py
+++ src/mlproject/exception.py
@@ -18,3 +18,9 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+    
+
+    
+    
+    
+   
